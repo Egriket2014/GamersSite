2cb07332e5780dca5781106f83c11503a445ba42
Egriket <egriket2014@yandex.ru>
2022-03-28 22:39:30 +0300

    Initial commit

---
+++ src/main/java/com/gamerssite/gamerssite/GamersSiteApplication.java
@@ -0,0 +1,13 @@
+package com.gamerssite.gamerssite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamersSiteApplication.class, args);
+    }
+
+}
